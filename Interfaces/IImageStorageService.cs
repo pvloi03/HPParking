8392c47afa945d600395174c1906ad8147872d34
phanvanloi <phanloi150203@gmail.com>
2026-09-26 14:28:32 +0700

    refactor: remove HPParking project configuration and entry point files

--- Interfaces/IImageStorageService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace HPParking.Interfaces
-{
-    public interface IImageStorageService
-    {
-        string SaveImage(Bitmap bitmap, string type, string folder, string basePath);
-    }
-}
